4fbfb38a82ba20aa2fdadf8da33f0e6b41c9122d
Dylan-sci <112603393+Dylan-sci@users.noreply.github.com>
2022-08-31 23:10:34 -0500

    Add files via upload

---
+++ Lab 8 Circular Queue/CircularQueueInterface.java
@@ -0,0 +1,10 @@
+package circularqueue;
+
+public interface CircularQueueInterface<E>
+{
+    public int size();
+    public boolean isEmpty();
+    public void enqueue(E data);
+    public E front() throws Exception;
+    public E dequeue() throws Exception;
+}
